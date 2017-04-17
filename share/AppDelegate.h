64a45c87350b240e4f50a4f6e6019f53ce052915
Mobile Innovation, LLC <service-in@mobile-innovation.jp>
2017-04-17 14:44:59 +0900

    Initial Commit

---
+++ share/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  share
+//
+//  Created by M.Amatani on 2017/04/17.
+//  Copyright © 2017年 Mobile Innovation. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ share/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  share
+//
+//  Created by M.Amatani on 2017/04/17.
+//  Copyright © 2017年 Mobile Innovation. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
